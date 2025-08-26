d280e235939d06f3239cd6c8d6277d77ce990fc5
Davincecodeking <ogunkorodevincent3@gmail.com>
2025-08-26 10:33:06 +0100

    feat: modify config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-     "./src/**/*.{html,ts}",
-     "./node_modules/tw-elements/dist/js/**/*.js"
+     "./src/**/*.{html,ts}"
   ],
   theme: {
     extend: {},
